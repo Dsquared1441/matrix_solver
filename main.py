96fe974343a4abfaacb5a18477d4b833f4060872
Dsquared1441 <116605437+Dsquared1441@users.noreply.github.com>
2022-10-29 12:00:47 -0400

    Add files via upload
    Given an arbitrary matrix, the program can solve it using row reduction.

---
+++ main.py
@@ -0,0 +1,104 @@
+'''
+	OK, so I fixed it and it works properly, now
+	
+	Additional improvement to make:
+	- solving matrix gives really wierd float numbers -- like 4e-28 when it is supposed to be 0 and 1.000000000000000000000 for 1
+		- make element type in matrix Decimal
+		- should I convert back to int/float?
+'''
+
+from matrix import Matrix
+
+# finds the pivot col of a 
+def findPivot(row):
+    for (i, x) in enumerate(row):
+        if x != 0:
+            return i
+
+# zeroes out cols by finding nonzero entries below pivot, scaling it, and adding it           
+def zeroOutLowerCols(mat, row, col):
+    rowmax = mat.size[0];
+    for r in range(row+1, rowmax):
+        if mat.matrix[r][col] != 0:
+            target = mat.matrix[r][col];
+            mat.scale(r, 1/target);
+            mat.replace(row, r);
+
+# does same thing as zeroOutLowerCols(), but does it for upper cols
+def zeroOutUpperCols(mat, row, col):
+    for r in range(row-1, -1, -1):
+        if mat.matrix[r][col] != 0:
+            target = mat.matrix[r][col];
+            mat.scale(r, -1/target);
+            mat.replace(row, r);
+            mat.scale(r, -target);
+
+# checks if matrix is all zeroes
+def isZeroMatrix(lists):
+    for i in lists:
+        for j in i:
+            if j != 0:
+                return False
+    return True
+
+# solves a matrix in RREF
+def solve(matrix):
+    # copy matrix
+    mat = matrix.copy();
+    if isZeroMatrix(mat.matrix[:]):
+        return 'Zero matrix'
+    
+    # init vars
+    row = 0;
+    row_max = mat.size[0];
+    
+    while (row < row_max):
+        # find pivot coordinates (check if zero row first)
+        
+        # if zero row, push list to bottom and pull other lists up
+        if mat.isZeroRow(row):
+            mat.matrix.append(mat.matrix[row])
+            mat.matrix.pop(row)
+            if isZeroMatrix(mat.matrix[row:]):
+                print('Free solution detected...')
+                break
+            continue
+        else:
+            piv_row = row;
+            piv_col = findPivot(mat.matrix[row]);
+        
+        # scale pivot coordinates
+        pivot = mat.matrix[piv_row][piv_col];
+        mat.scale(piv_row, -1/pivot);
+        
+        # make col all zeroes
+        zeroOutLowerCols(mat, piv_row, piv_col);
+
+        mat.scale(row, -1);
+        
+        # increment row counter
+        row += 1;
+        
+    row = row_max - 1;
+    
+    # now find most recent pivot and make all upper cols zero
+    while (row > -1):
+        
+        # reverse find pivot (check if zero first)
+        
+        if mat.isZeroRow(row):
+            row -= 1;
+            continue
+        
+        piv_row = row;
+        piv_col = findPivot(mat.matrix[row]);
+        
+        # zero out upper col
+        zeroOutUpperCols(mat, piv_row, piv_col);
+        
+        row -= 1;
+        
+    print(mat)
+
+
+    
---
+++ matrix.py
@@ -0,0 +1,62 @@
+print("mat.py imported.")
+
+class Matrix():
+    
+    '''
+        rows -- a list of rows for the matrix
+    '''
+    def __init__(self, rows):
+        self.matrix = rows;
+        self.size = [len(rows), len(rows[0])];
+        self.row_len = len(rows[0])
+    
+    # checks if row n is all zeroes    
+    def isZeroRow(self, n):
+        for i in range(0, self.row_len):
+            if self.matrix[n][i] != 0:
+                return False
+        return True
+        
+    # scales row row by a factor of n
+    def scale(self, row, n):
+        for i in range(0, self.row_len):
+            self.matrix[row][i] = self.matrix[row][i] * n;
+            
+    # swaps row row1 with row row2
+    def swap(self, row1, row2):
+        a = self.matrix[row1][:]
+        b = self.matrix[row2][:]
+        self.matrix[row1] = b;
+        self.matrix[row2] = a;
+    
+    # adds the entries from row r1 into row r2, and stores them into row r2    
+    def replace(self, r1, r2):
+        row1 = self.matrix[r1];
+        row2 = self.matrix[r2];
+        for i in range(0, self.row_len):
+            row2[i] = row2[i] + row1[i];
+            if (row2[i] < 1e-6):
+                row2[i] = 0;
+        self.matrix[r2] = row2;
+
+    # creates a copy of the matrix
+    def copy(self):
+        a = []
+        for i in range(0, self.size[0]):
+            b = [];
+            for j in range(0, self.size[1]):
+                b.append(self.matrix[i][j]);
+            a.append(b)
+        return Matrix(a)
+        
+    def __str__(self):
+        s = '\n';
+        for i in self.matrix:
+            s += str(i) + '\n';
+        return s
+            
+    def __repr__(self):
+        s = '\n';
+        for i in self.matrix:
+            s += str(i) + '\n';
+        return s
