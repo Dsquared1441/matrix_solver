289087e7b2eff4ae7920faeb5fdcd6261392c8f2
Dsquared1441 <116605437+Dsquared1441@users.noreply.github.com>
2022-11-10 23:29:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 	Additional improvement to make (DONE):
 	- solving matrix gives really wierd float numbers -- like 4e-28 when it is supposed to be 0 and 1.000000000000000000000 for 1
 		- used floating point precision of 1e-6 to solve round-off errors when adding numbers
+
+		Nov 10, 2022
+		-gave program a terminal user interface
+		-fixed precision so it is for any arbitray number (x - dx > precision)
 '''
 
 from matrix import Matrix
@@ -99,5 +103,86 @@ def solve(matrix):
         
     print(mat)
 
-
+# gets the maximum error the computer can make while doing matrix arithmetic
+def get_precision():
+    while True:
+        try:
+            a = input("Please enter the precision as a power of ten (ex: 1e-6 or 1e-9): ")
+            a = float(a)
+            return a
+        except Exception:
+            print("Invalid input, please try again...")
+            
+# prints main menu choices
+def getChoices():
+    print("""Press:
+            R to row-reduce a matrix or
+            Q to quit the program\n""")
+    a = input()
+    return a.upper()
+            
+# gets the matriix in rows, and keeps on prompting until they get the right matrix
+def getMatrix():
+    mat = [[]]
+    i = 0;
+    print("Enter rows as numbers separated by commas and spaces, and press enter when done (ex: 1, 2.5, 3 <enter>)")
+    print("Press enter when done")
+    while True:
+        a = input("Row {}:\t".format(i+1))
+        # if the user wants to quit, let them
+        if a.isspace() or len(a) == 0:
+            mat.pop()
+            break
+        # process matrix
+        else:
+            strings = a.split(', ')
+            if (i != 0) and (len(strings) != len(mat[i-1])):
+                print("Improper number of entries in row or wrong format...try again")
+                continue
+            else:
+                try:
+                    for s in strings:
+                        mat[i].append(float(s))
+                    mat.append([])
+                    i = i+1;
+                except ValueError:
+                    print('Enter only numbers, please try again...')
+        
+        
+    return mat            
+    
+# main code block
+if __name__ == '__main__':
+    print("Matrix Solver")
+    print("---"*10)
+    print()
+    PREC = get_precision();
+    
+    print()
+    while True:
+        choice = getChoices()
+        # reduce matrix, so get matrix and solve
+        if choice == 'R':
+            mat = getMatrix()
+            if len(mat[0]) == 0:
+                print('uh oh, you didn\'t enter a matrix! Restarting...')
+                continue
+            mat = Matrix(mat, precision=PREC)
+            print("\nThe input matrix:")
+            print(mat)
+            print()
+            print("\nThe row reduced echelon form of the matrix:")
+            solve(mat)
+            
+        # quit
+        elif choice == 'Q':
+            print("Thanks for using me!")
+            break
+        
+        # invalid coice
+        else:
+            print("Invalid choice, please enter R or Q...")
+    print("goodbye!")    
+    
+    
     
